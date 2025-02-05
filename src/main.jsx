5f2089499b8105c2139e91c1ecc7d1b8f490f919
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-05 16:28:10 +0545

    add dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import Sign_Up from "./pages/Sign_Up.jsx";
 import Reset_Password from "./pages/Reset_Password.jsx";
 import Sign_In from "./pages/Sign_In.jsx";
 import Blog_Section from "./pages/Blog_Section.jsx";
-import Footer from "./components/Footer.jsx";
 import Product_Description from "./pages/Product_Description.jsx";
 import Docs from "./pages/Docs.jsx";
 import Top_Navbar from "./components/Top_Navbar.jsx";
@@ -20,6 +19,7 @@ import Mega_Menu from "./pages/Mega_Menu.jsx";
 import Stores from "./pages/Stores.jsx";
 import Shop from "./pages/Shop.jsx";
 import Single_Category from "./pages/Single_Category.jsx";
+import Footer from "./components/Footer.jsx";
 // import Signin_Card from "./pages/signin_card.jsx";
 
 export function Root(){
