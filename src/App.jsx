4735de7e6d64830ca11de8c95459d1f0208c5bca
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-06 08:30:14 +0545

    remove dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero_Section_Slider from "./components/hero_section_slider";
+// import Hero_Section_Slider from "./components/hero_section_slider";
 import Banner_Section from "./components/Banner_Section";
 import Popular_Product_Section from "./components/Popular_Product_Section";
 import Home_Morden from "./components/Home_Morden";
@@ -14,7 +14,7 @@ export default function App() {
     <div className=" space-y-4">
    
      
-   <Hero_Section_Slider />
+   {/* <Hero_Section_Slider /> */}
       <Feature_Categories_Section/>
       <Banner_Section />
       <Popular_Product_Section />
