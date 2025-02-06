59ddf00f81feee7c86640671f4da66e62c427cdb
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-06 08:32:02 +0545

    remove dashboard

--- src/components/Popular_Product_Section.jsx
+++ src/components/Popular_Product_Section.jsx
@@ -13,7 +13,7 @@ import eleven from "../../public/popularproducts/product11.jpg";
 import twelve from "../../public/popularproducts/product12.jpg";
 import thirteen from "../../public/popularproducts/product13.jpg";
 
-import Single_Product_Card from "./Single_Product_card";
+// import Single_Product_Card from "./Single_Product_card";
 
 export default function Popular_Product_Section() {
   return (
@@ -21,9 +21,9 @@ export default function Popular_Product_Section() {
       <div className=" w-10/12 mx-auto  space-y-8 mt-24 ">
         <h1 className="text-2xl font-semibold opacity-70 ">Popular Products</h1>
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4 space-y-1.5">
-          {Products.map((item, index) => (
+          {/* {Products.map((item, index) => (
             <Single_Product_Card item={item} key={index}/>
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
