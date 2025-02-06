6d4e28490dc8d6fb8bcef52d7aa9ff4b317cbc38
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-06 16:30:33 +0545

    Finally Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Banner_Section from "./components/Banner_Section";
 import Popular_Product from "./components/Popular_Product";
 import Modern_Section from "./components/Modern_Section";
 import Icons_Section from "./components/Icons_Section";
+import Hero_Section from "./components/Hero_Section";
 
 export default function App() {
   return (
     <div className=" space-y-4">
-     
+     <Hero_Section/>
       <Feature_Category />
       <Banner_Section />
       <Popular_Product />
