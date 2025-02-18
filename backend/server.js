69042093fc86568e2008d475dfff85749acbec4c
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-18 13:06:48 +0545

    creat section add

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,54 @@
 import express from "express";
-// import { Database } from "lucide";
-// import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // Configure the server
 const app = express();
+app.use(express.json());
+
+// Connect to MongoDB Database
+
+try {
+  mongoose.connect(
+    "mongodb+srv://riteshtamang1027:rKyz6jdB5VveFK8U@cluster0.k0oe9.mongodb.net/ecommerce-db?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log("MongoDB Connection Success");
+} catch (error) {
+  console.log("MongoDB Connection Error", error);
+}
+
+// Product Schema(items for the product table)
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: false },
+  price: { type: Number, required: true },
+  previousprice: { type: Number, required: true },
+  imgUrl: { type: String, required: true },
+  category: { type: String, required: true },
+});
+
+// Make product Table
+
+const product = mongoose.model("product", productSchema);
+
+// CRUDE for product
+
+//1. Create a product
+app.post("/products", async (req, res) => {
+
+  try {
+const newproduct= await new product(req.body).save();
+console.log(newproduct)
+
+  } catch (error) {
+
+    console.log("Something went worng",error);
+  }
+});
+
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Server is working...");
