1d7869d1cef38fe2cd8ed22071347af352c507ff
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-25 18:56:57 +0545

    fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect, useState } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
