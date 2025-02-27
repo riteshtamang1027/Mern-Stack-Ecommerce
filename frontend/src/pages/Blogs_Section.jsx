9b3cce6f7e519869396364aee824ac4b8d027fb3
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-27 12:46:52 +0545

    fixed

--- frontend/src/pages/Blogs_Section.jsx
+++ frontend/src/pages/Blogs_Section.jsx
@@ -56,29 +56,7 @@ export default function Blogs_Section() {
           {Items.map((item, index) => (
             <Blog_Category_Section key={index} item={item} />
 
-            // <div key={index} className="flex flex-col max-md:w-full xl:w-[27vw] xl:h-[58vh]   relative  ">
-            //   <div className="max-md:w-full md:w-full rounded-xl object-cover overflow-hidden cursor-pointer  ">
-            //     <img
-            //       className="  hover:scale-110 duration-700"
-            //       src={item.Image}
-            //       alt="Image"
-            //     />
-            //   </div>
-            //   <div className="space-y-4 mt-4 ">
-            //     <p className="text-green-700 ">{item.Category}</p>
-            //     <p className="text-lg font-semibold  opacity-80 hover:text-green-800 cursor-pointer">
-            //       {item.Name}
-            //     </p>
-            //     <p className="opacity-60 ">{item.About_item}</p>
-            //     <div className="text-xs font-semibold flex justify-between item-center">
-            //       <p className="opacity-50">{item.Date}</p>
-            //       <p>
-            //         {" "}
-            //         <span className=" opacity-50">Read time:</span> {item.Time} <span className=" opacity-50">min</span>
-            //       </p>
-            //     </div>
-            //   </div>
-            // </div>
+            
           ))}
         </div>
       </div>
