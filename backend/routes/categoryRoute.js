c3c2fa7302168bf7c8c83d4467818cd8f10ad4cd
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-03-02 22:04:49 +0545

    remove .env

--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,6 +1,5 @@
 import express from "express";
 const router = express.Router();
-
 import multer from "multer";
 import { createCategory, deleteCategoryById, getAllcategories, getSinglecategoryById, updatedCategoryById } from "../controller/categoryController.js";
 const upload = multer({ dest: "uploads/" });
